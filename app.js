4f1ac6bc4e3f10cdf2da9b95a7eefd08c79ae407
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2023-10-05 17:54:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ function foodget(){
 }
 var deer = new Image(200, 200);
 			deer.src = "deer.png";
-			deer.style.zIndex = "200";
+			deer.style.zIndex = "2000";
 			deer.style.position = "absolute";
 deer.hidden = true;
 			document.body.appendChild(deer);
